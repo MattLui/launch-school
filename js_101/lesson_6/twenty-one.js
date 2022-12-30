104d2cd7dc2d6ad2e165b375d83a33baf2d6271f
Matthew Lui <mattclui@gmail.com>
2022-12-29 17:11:04 -0800

    twenty-one with bonus features

--- js_101/lesson_6/twenty-one.js
+++ js_101/lesson_6/twenty-one.js
@@ -1,10 +1,17 @@
+/* eslint-disable max-statements */
+/* eslint-disable max-lines-per-function */
 const readline = require('readline-sync');
+const GAMES_TO_WIN = 3;
+const TARGET_TOTAL = 21;
+const DEALER_STANDS_AT = 17;
+let playerScore = 0;
+let dealerScore = 0;
 
 function prompt(msg) {
   console.log(`=> ${msg}`);
 }
 
-//1. Initialize deck
+//Initialize deck
 let suits = ['S', 'H', 'D', 'C'];
 let ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
 let deck = [];
@@ -40,97 +47,133 @@ function total(cards) {
   });
 
   values.filter(value => value === 'A').forEach(_ => {
-    if (sum > 21) sum -= 10;
+    if (sum > TARGET_TOTAL) sum -= 10;
   });
 
   return sum;
 }
 
 function busted(total) {
-  return total > 21;
+  return total > TARGET_TOTAL;
 }
 
 function playAgain() {
   console.log('-------------');
-  prompt('Do you want to play again? (y or n)');
-  let answer = readline.question();
-  return answer.toLowerCase(0)[0] === 'y';
+  prompt('Would you like to play again? y/n');
+  let answer;
+  while (true) {
+    answer = readline.question().toLowerCase();
+    if (answer !== 'y' && answer !== 'n') prompt('Please enter a valid response. (y/n)');
+    else return answer;
+  }
 }
 
 function hand(cards) {
   return cards.map(card => `${card[1]}`).join(' ');
 }
 
+function displayScore() {
+  prompt(`Player's score: ${playerScore}`);
+  prompt(`Dealer's score: ${dealerScore}`);
+}
+
+//Main game loop
 while (true) {
-  prompt('Welcome to Twenty-One!');
+  playerScore = 0;
+  dealerScore = 0;
+  prompt(`Welcome to ${TARGET_TOTAL}!`);
+  prompt(`The goal is to try to get as close to ${TARGET_TOTAL} as possible without busting.`);
+  prompt(`The dealer will stand at ${DEALER_STANDS_AT} or above.`);
+  prompt(`First to win ${GAMES_TO_WIN} games wins the match. Press [ENTER] to begin.`);
+  readline.question();
+  playRound();
+  if (playAgain() === "n") break;
+}
 
-  shuffle(deck);
-
-  //2. Deal cards to player and dealer
-  let playerHand = [deck.pop(), deck.pop()];
-  let dealerHand = [deck.pop(), deck.pop()];
-  let playerTotal = total(playerHand);
-  let dealerTotal = total(dealerHand);
-  prompt(`Dealer has ${dealerHand[0][1]} and ?`);
-  prompt(`Player has ${playerHand[0][1]} and ${playerHand[1][1]}`);
-  prompt(`Player total: ${playerTotal}`);
-
-  /*3. Player turn: hit or stay
-    - repeat until bust or stay*/
+function playRound() {
   while (true) {
-    let answer;
+    console.clear();
+    shuffle(deck);
+
+    //Deal cards to player and dealer
+    let playerHand = [deck.pop(), deck.pop()];
+    let dealerHand = [deck.pop(), deck.pop()];
+    let playerTotal = total(playerHand);
+    let dealerTotal = total(dealerHand);
+    prompt(`Dealer has ${dealerHand[0][1]} and ?`);
+    prompt(`Player has ${playerHand[0][1]} and ${playerHand[1][1]}`);
+    prompt(`Player total: ${playerTotal}`);
+
+    /*Player turn: hit or stay
+      - repeat until bust or stay*/
     while (true) {
-      prompt('Hit or stay? (h or s)');
-      answer = readline.question().toLowerCase();
-      if (['h', 's'].includes(answer)) break;
-      prompt("Please enter 'h' or 's'.");
+      let answer;
+      while (true) {
+        prompt('Hit or stay? (h or s)');
+        answer = readline.question().toLowerCase();
+        if (['h', 's'].includes(answer)) break;
+        prompt("Please enter 'h' or 's'.");
+      }
+
+      if (answer === 'h') {
+        playerHand.push(deck.pop());
+        playerTotal = total(playerHand);
+        prompt(`Player is dealt ${playerHand[playerHand.length - 1][1]}. Player total: ${playerTotal}`);
+      }
+
+      if (busted(playerTotal) || answer === 's') break;
     }
 
-    if (answer === 'h') {
-      playerHand.push(deck.pop());
-      playerTotal = total(playerHand);
-      console.log(`Player is dealt ${playerHand[playerHand.length - 1][1]}. Player total: ${playerTotal}`);
-    }
-
-    if (busted(playerTotal) || answer === 's') break;
-  }
-
-  //4. If player busts, dealer wins.
-  if (busted(playerTotal)) {
-    prompt('You busted! Dealer wins.');
-  } else {
-    prompt("You chose to stay!");
-
-    /*5. Dealer turn: hit or stay
-    - repeat until total >= 17*/
-    prompt(`Dealer's cards are revealed. Dealer has ${hand(dealerHand)}.`);
-    while (true) {
-      if (busted(dealerTotal) || dealerTotal >= 17) break;
-      dealerHand.push(deck.pop());
-      dealerTotal = total(dealerHand);
-      prompt(`Dealer hits: ${dealerHand[dealerHand.length - 1][1]}`);
-    }
-
-    //6. If dealer busts, player wins.
-    if (busted(dealerTotal)) {
-      console.log(`Dealer has ${dealerTotal} and busts! You win.`);
+    //If player busts, dealer wins.
+    if (busted(playerTotal)) {
+      prompt('You busted! Dealer wins.');
+      dealerScore += 1;
     } else {
-      console.log(`Dealer stays at ${dealerTotal}.`);
-    }
-  }
+      prompt("You chose to stay!");
 
-  //7. Compare cards and declare winner.
-  if (!(busted(playerTotal) || busted(dealerTotal))) {
-    prompt(`Dealer's total: ${dealerTotal}`);
-    prompt(`Player's total: ${playerTotal}`);
-    if (playerTotal > dealerTotal) {
-      prompt('You win!');
-    } else if (playerTotal < dealerTotal) {
-      prompt('Dealer wins.');
-    } else {
-      prompt ("It's a push!");
-    }
-  }
+      /*Dealer turn: hit or stay
+      - repeat until total >= DEALER_STANDS_AT*/
+      prompt(`Dealer's cards are revealed. Dealer has ${hand(dealerHand)}.`);
+      while (true) {
+        if (busted(dealerTotal) || dealerTotal >= DEALER_STANDS_AT) break;
+        dealerHand.push(deck.pop());
+        dealerTotal = total(dealerHand);
+        prompt(`Dealer hits: ${dealerHand[dealerHand.length - 1][1]}`);
+      }
 
-  if (!playAgain()) break;
+      //If dealer busts, player wins.
+      if (busted(dealerTotal)) {
+        prompt(`Dealer has ${dealerTotal} and busts! You win.`);
+        playerScore += 1;
+      } else {
+        prompt(`Dealer stays at ${dealerTotal}.`);
+      }
+    }
+
+    //Compare cards and declare winner.
+    if (!(busted(playerTotal) || busted(dealerTotal))) {
+      prompt(`Dealer's total: ${dealerTotal}`);
+      prompt(`Player's total: ${playerTotal}`);
+      if (playerTotal > dealerTotal) {
+        prompt('You win!');
+        playerScore += 1;
+      } else if (playerTotal < dealerTotal) {
+        prompt('Dealer wins.');
+        dealerScore += 1;
+      } else {
+        prompt ("It's a push.");
+      }
+    }
+
+    displayScore();
+    if (playerScore === GAMES_TO_WIN) {
+      prompt('Congraulations. You won the match!');
+      break;
+    } else if (dealerScore === GAMES_TO_WIN) {
+      prompt('The dealer wins the match.');
+      break;
+    }
+    prompt('Press [ENTER] to continue.');
+    readline.question();
+  }
 }
